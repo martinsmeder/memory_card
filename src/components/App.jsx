a420b710ce489532e830e4dcfe1a6f3dc22a6c50
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-21 10:47:47 +0200

    Added notes and project plan

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <>
-      <h1>Hi mom!</h1>
-    </>
-  );
-}
+import { useState, useEffect } from 'react';
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   useEffect(() => {
+//     const key = setInterval(() => {
+//       setCounter((count) => count + 1);
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(key);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       <h1>Hi mom!</h1>
+//       <p>{counter} seconds have passed.</p>
+//     </>
+//   );
+// }
 
 export default App;
