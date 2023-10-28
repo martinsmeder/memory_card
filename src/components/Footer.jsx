653144e170b5d2c58432d90ff9bfaec5968bf3be
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-28 14:28:55 +0200

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer>
-      <a target='_blank' href='https://www.pexels.com'>
-        Photos provided by Pexels API
+      <a target='_blank' href='https://pixabay.com/'>
+        Photos provided by Pixabay API
       </a>
     </footer>
   );
