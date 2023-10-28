40969a0882e111eea2d76207d5597cf29250bcca
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-28 07:59:37 +0200

    Setup vite.config.js for environment variables

--- src/components/api.js
+++ src/components/api.js
@@ -1,32 +1,32 @@
-// import { createClient } from 'pexels';
+import { createClient } from 'pexels';
 
-// function parse(arr) {
-//   return arr.map((photo) => {
-//     return {
-//       title: photo.alt,
-//       image: photo.src.large,
-//       clicks: 0,
-//     };
-//   });
-// }
+function parse(arr) {
+  return arr.map((photo) => {
+    return {
+      title: photo.alt,
+      image: photo.src.large,
+      clicks: 0,
+    };
+  });
+}
 
-// function removeDuplicates(arr) {
-//   const titles = [];
-//   return arr.filter((item) => {
-//     if (!titles.includes(item.title)) {
-//       titles.push(item.title);
-//       return true;
-//     }
-//     return false;
-//   });
-// }
+function removeDuplicates(arr) {
+  const titles = [];
+  return arr.filter((item) => {
+    if (!titles.includes(item.title)) {
+      titles.push(item.title);
+      return true;
+    }
+    return false;
+  });
+}
 
-// export default function getPhotos(query, orientation) {
-//   const apiKey = import.meta.env.VITE_API_KEY;
-//   const client = createClient(apiKey);
+export default function getPhotos(query, orientation) {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const client = createClient(apiKey);
 
-//   return client.photos
-//     .search({ query, orientation, per_page: 50 })
-//     .then((response) => parse(response.photos))
-//     .then((parsed) => removeDuplicates(parsed));
-// }
+  return client.photos
+    .search({ query, orientation, per_page: 50 })
+    .then((response) => parse(response.photos))
+    .then((parsed) => removeDuplicates(parsed));
+}
