b6c87c21bb5255f223092776b389d2a9b8816cc7
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-28 09:32:14 +0200

    Checking to see if pexels API is causing issues

--- src/components/api.js
+++ src/components/api.js
@@ -1,32 +1,45 @@
-import { createClient } from 'pexels';
+// import { createClient } from 'pexels';
 
-function parse(arr) {
-  return arr.map((photo) => {
-    return {
-      title: photo.alt,
-      image: photo.src.large,
-      clicks: 0,
-    };
-  });
-}
+// function parse(arr) {
+//   return arr.map((photo) => {
+//     return {
+//       title: photo.alt,
+//       image: photo.src.large,
+//       clicks: 0,
+//     };
+//   });
+// }
 
-function removeDuplicates(arr) {
-  const titles = [];
-  return arr.filter((item) => {
-    if (!titles.includes(item.title)) {
-      titles.push(item.title);
-      return true;
-    }
-    return false;
-  });
-}
+// function removeDuplicates(arr) {
+//   const titles = [];
+//   return arr.filter((item) => {
+//     if (!titles.includes(item.title)) {
+//       titles.push(item.title);
+//       return true;
+//     }
+//     return false;
+//   });
+// }
 
-export default function getPhotos(query, orientation) {
-  const apiKey = 'vJLwQWKyEUXijSGq9mdr0doBLdNVLV5VWkjarRO4stbszvCtcwvuIzX9';
-  const client = createClient(apiKey);
+// export default function getPhotos(query, orientation) {
+//   const apiKey = 'vJLwQWKyEUXijSGq9mdr0doBLdNVLV5VWkjarRO4stbszvCtcwvuIzX9';
+//   const client = createClient(apiKey);
 
-  return client.photos
-    .search({ query, orientation, per_page: 50 })
-    .then((response) => parse(response.photos))
-    .then((parsed) => removeDuplicates(parsed));
+//   return client.photos
+//     .search({ query, orientation, per_page: 50 })
+//     .then((response) => parse(response.photos))
+//     .then((parsed) => removeDuplicates(parsed));
+// }
+
+export default function getImages(numberOfImages, width, height) {
+  const images = [];
+
+  for (let i = 0; i < numberOfImages; i++) {
+    // Generate a URL for a random image with the specified width and height
+    const imageUrl = `https://picsum.photos/${width}/${height}?random=${i}`;
+
+    images.push({ title: `Image ${i + 1}`, image: imageUrl, clicks: 0 });
+  }
+
+  return images;
 }
